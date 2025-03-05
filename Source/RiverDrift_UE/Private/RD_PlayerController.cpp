0c57a444a3b1a4f66327671f49a1452b2f94bbc5
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2025-03-05 16:11:24 -0700

    init creation of player input

---
+++ Source/RiverDrift_UE/Private/RD_PlayerController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RD_PlayerController.h"
+
+void ARD_PlayerController::BeginPlay()
+{
+
+	SetShowMouseCursor(true);
+}
