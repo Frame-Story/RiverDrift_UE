bcedabee489a7ff1339e59413c46122c433be870
DESKTOP-V2D20DK\ryand <ryan.davidson.dev@proton.me>
2025-11-05 12:50:33 -0700

    fixed hud not updating, pfp update, and field gen

--- Source/RiverDrift_UE/Private/Core/RD_GameMode.cpp
+++ Source/RiverDrift_UE/Private/Core/RD_GameMode.cpp
@@ -38,31 +38,32 @@ void ARD_GameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
-		UE_LOG(LogTemp, Warning, TEXT("ctf hi"))
+		//UE_LOG(LogTemp, Warning, TEXT("ctf hi"))
+	
+	OnGameModeInitializedDelegate.Broadcast();
 
-	// Load the package first
-	UPackage* Package = LoadPackage(nullptr, TEXT("/Script/RiverDrift_UE"), LOAD_None);
-	if (!Package)
-	{
-		UE_LOG(LogTemp, Error, TEXT("Failed to load /Script/RiverDrift_UE package!"));
-		return;
-	}
+	//// Load the package first
+	//UPackage* Package = LoadPackage(nullptr, TEXT("/Script/RiverDrift_UE"), LOAD_None);
+	//if (!Package)
+	//{
+	//	UE_LOG(LogTemp, Error, TEXT("Failed to load /Script/RiverDrift_UE package!"));
+	//	return;
+	//}
 
-	// Now force the struct to be registered
-	(void)FDialogueQuestLookup::StaticStruct();
+	//// Now force the struct to be registered
+	//(void)FDialogueQuestLookup::StaticStruct();
 
-	// Test
-	if (const UScriptStruct* Struct = FindObject<UScriptStruct>(ANY_PACKAGE, TEXT("FDialogueQuestLookup")))
-	{
-		UE_LOG(LogTemp, Warning, TEXT("ctf Found struct: %s"), *Struct->GetName());
-	}
-	else
-	{
-		UE_LOG(LogTemp, Error, TEXT("ctf Struct still not found!"));
-	}
+	//// Test
+	//if (const UScriptStruct* Struct = FindObject<UScriptStruct>(ANY_PACKAGE, TEXT("FDialogueQuestLookup")))
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("ctf Found struct: %s"), *Struct->GetName());
+	//}
+	//else
+	//{
+	//	UE_LOG(LogTemp, Error, TEXT("ctf Struct still not found!"));
+	//}
 
 	//PrototypingManagerInstance =  GetWorld()->SpawnActor<ARDPrototypingManager>(PrototypingManagerFormat);
-	OnGameModeInitializedDelegate.Broadcast();
 }
 
 
